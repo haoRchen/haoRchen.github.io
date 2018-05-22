e7ecbbc624e886a7df226c24b707e13c849e5fae
haoRchen <hao.ran.chen24@gmail.com>
2018-05-22 14:47:32 -0400

    Added css related libraries

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "babel-polyfill"; // Needed to support IE11 & Safari 9
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App)
