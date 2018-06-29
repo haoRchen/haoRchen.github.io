e885723cd40efcced476460e13ae350ae103a305
haoRchen <hao.ran.chen24@gmail.com>
2018-06-29 07:11:09 -0400

    modal x icon added, prior to using static modal

--- src/components/PortfolioComponents/ProjectCard.vue
+++ src/components/PortfolioComponents/ProjectCard.vue
@@ -108,9 +108,9 @@ export default {
         ProjectDetail,
         { project: this.project },
         {
-          scrollable: true,
-          // width: "100%"
-          height: "auto"
+          // scrollable: true,
+          transition: "fade"
+          // height: "auto"
         }
       );
     }
--- src/components/PortfolioComponents/ProjectDetail.vue
+++ src/components/PortfolioComponents/ProjectDetail.vue
@@ -1,56 +1,64 @@
 <template>
-  <div class="modal">
-    <a
-      class="icon is-large modal__close"
-      @click="$emit('close')">
-      <i 
-        class="fas fa-times fa-3x"
-        aria-hidden="true"/>
-    </a>
-    <h5 class="modal__title">
-      {{ project.name }}
-    </h5>
-    <carousel 
-      :per-page="1"
-      :pagination-enabled="false"
-      :navigation-enabled="true"
-      :navigation-next-label="`<i class='fas fa-chevron-right fa-2x'></i>`"
-      :navigation-prev-label="`<i class='fas fa-chevron-left fa-2x'></i>`"
-      :easing="'ease-out'"
-      class="modal__carousel" >
-      <slide class="modal__carousel__slide">
-        <img 
-          class="modal__image" 
-          src="Images/portfolio/1.jpg" 
-          alt="">
-      </slide>
-      <slide class="modal__carousel__slide">
-        <img 
-          class="modal__image" 
-          src="Images/portfolio/2.jpg" 
-          alt="">
-      </slide>
-      <slide class="modal__carousel__slide">
-        <img 
-          class="modal__image" 
-          src="Images/portfolio/3.jpg" 
-          alt="">
-      </slide>
-      <slide class="modal__carousel__slide">
-        <img 
-          class="modal__image" 
-          src="Images/portfolio/4.jpg" 
-          alt="">
-      </slide>
-    </carousel>
-    <p class="modal__description">
-      {{ project.fullDescription }}
-    </p>
-    <h6><strong>Technologies used</strong></h6>
-    <p class="modal__technologies">
-      Reactjs  -  JavaScript  -  CSS3  -  Bulma
-    </p>
-  </div>
+  <transition name="slide-fade">
+    <div class="modal">
+      <div 
+        class="modal__close" 
+        @click="$emit('close')">
+        <span class="modal__close__line1"/>
+        <span class="modal__close__line2"/>
+      </div>
+      <!-- <a
+        class="icon is-large modal__close"
+        @click="$emit('close')">
+        <i 
+          class="fas fa-times fa-3x"
+          aria-hidden="true"/>
+      </a> -->
+      <h5 class="modal__title">
+        {{ project.name }}
+      </h5>
+      <!-- <carousel 
+        :per-page="1"
+        :pagination-enabled="false"
+        :navigation-enabled="true"
+        :navigation-next-label="`<i class='fas fa-chevron-right fa-2x'></i>`"
+        :navigation-prev-label="`<i class='fas fa-chevron-left fa-2x'></i>`"
+        :easing="'ease-out'"
+        class="modal__carousel" >
+        <slide class="modal__carousel__slide">
+          <img 
+            class="modal__image" 
+            src="Images/portfolio/1.jpg" 
+            alt="">
+        </slide>
+        <slide class="modal__carousel__slide">
+          <img 
+            class="modal__image" 
+            src="Images/portfolio/2.jpg" 
+            alt="">
+        </slide>
+        <slide class="modal__carousel__slide">
+          <img 
+            class="modal__image" 
+            src="Images/portfolio/3.jpg" 
+            alt="">
+        </slide>
+        <slide class="modal__carousel__slide">
+          <img 
+            class="modal__image" 
+            src="Images/portfolio/4.jpg" 
+            alt="">
+        </slide>
+      </carousel> -->
+      <p class="modal__description">
+        {{ project.fullDescription }}
+      </p>
+      <h6><strong>Technologies used</strong></h6>
+      <p class="modal__technologies">
+        Reactjs  -  JavaScript  -  CSS3  -  Bulma
+      </p>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -83,25 +91,61 @@ export default {
 @import "@/main.scss";
 .modal {
   display: flex;
-  background-color: white;
+  // background-color: white;
   flex-direction: column;
   justify-content: flex-start;
   height: auto;
   align-items: center;
   overflow-y: auto;
   &__close {
+    $close: &;
+    padding: 1.5rem;
     color: $dark-grey;
     align-self: flex-end;
-    position: fixed;
+    position: absolute;
+    cursor: pointer;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    &__line1,
+    &__line2 {
+      position: relative;
+      height: 4px;
+      width: 35px;
+      background-color: $dark-grey;
+      border-radius: 15px;
+      margin: 2.5px;
+      transition: all ease-in-out 0.3s;
+    }
+    &__line1 {
+      top: 9px;
+      transform: rotate(45deg);
+    }
+    &__line2 {
+      transform: rotate(135deg);
+    }
     &:hover {
-      color: $primary-dark;
+      & #{ $close }__line1 {
+        transform: rotate(135deg);
+      }
+      & #{ $close }__line2 {
+        transform: rotate(45deg);
+      }
+    }
+    &:active {
+      & #{ $close }__line1,
+      & #{ $close }__line2 {
+        transform: none;
+      }
     }
   }
   &__title {
     font-family: $roboto;
     font-weight: 600;
     font-size: 2rem;
-    margin: 2rem;
+    margin: 3rem;
   }
   &__image {
     width: 50%;
